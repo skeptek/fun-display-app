b348eca70bf26a0ce78bf012a2589327bf50191f
Keith Forsythe <kforsythe76@gmail.com>
2017-01-22 15:27:42 -0500

    building form and display obj

---
+++ app/src/main/java/com/skeptek/fundisplayapp/DisplayText.java
@@ -0,0 +1,17 @@
+package com.skeptek.fundisplayapp;
+
+/**
+ * Created by tor611 on 1/22/17.
+ */
+public class DisplayText {
+
+    private String mDisplayText;
+
+    public void setmDisplayText(String mDisplayText) {
+        this.mDisplayText = mDisplayText;
+    }
+
+    public String getmDisplayText() {
+        return this.mDisplayText;
+    }
+}
